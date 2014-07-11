e618cd51654b92b21f98101aed311eee31636b76
akonovalova <anastasya.konovalova@intexsoft.by>
2014-07-11 18:50:29 +0300

    Base settings commit

---
+++ CrittercismApp/src/main/java/intexsoft/by/crittercismapi/ui/activity/MainActivity.java
@@ -0,0 +1,14 @@
+package intexsoft.by.crittercismapi.ui.activity;
+
+import android.app.Activity;
+import intexsoft.by.crittercismapi.R;
+import org.androidannotations.annotations.EActivity;
+
+/**
+ * Created by anastasya.konovalova on 11.07.2014.
+ */
+@EActivity(R.layout.activity_main)
+public class MainActivity extends Activity
+{
+
+}
