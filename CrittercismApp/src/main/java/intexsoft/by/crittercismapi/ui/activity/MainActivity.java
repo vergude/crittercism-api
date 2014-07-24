6bddaf561d8a48937d6c0ea21b1ee226e46158bf
Onregs <vadimikorzun@yandex.ru>
2014-07-24 13:14:26 +0300

    NavigationDrawer

--- CrittercismApp/src/main/java/intexsoft/by/crittercismapi/ui/activity/MainActivity.java
+++ CrittercismApp/src/main/java/intexsoft/by/crittercismapi/ui/activity/MainActivity.java
@@ -1,8 +1,12 @@
 package intexsoft.by.crittercismapi.ui.activity;
 
 import android.app.Activity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 import intexsoft.by.crittercismapi.R;
 import intexsoft.by.crittercismapi.data.facade.RemoteFacade;
+import intexsoft.by.crittercismapi.ui.adapters.NavigationDrawerAdapter;
 import intexsoft.by.crittercismapi.ui.presenter.MainPresenter;
 import intexsoft.by.crittercismapi.ui.presenter.MainPresenterImpl;
 import intexsoft.by.crittercismapi.ui.view.BaseView;
@@ -10,6 +14,8 @@ import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Background;
 import org.androidannotations.annotations.Bean;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ItemClick;
+import org.androidannotations.annotations.ViewById;
 
 /**
  * Created by anastasya.konovalova on 11.07.2014.
@@ -17,6 +23,9 @@ import org.androidannotations.annotations.EActivity;
 @EActivity(R.layout.activity_main)
 public class MainActivity extends Activity implements BaseView
 {
+    @ViewById(R.id.main_left_drawer)
+    ListView leftDrawer;
+
 	@Bean(MainPresenterImpl.class)
 	MainPresenter presenter;
 
@@ -44,7 +53,25 @@ public class MainActivity extends Activity implements BaseView
 		presenter.init(this);
 
 		getApps();
+
 	}
+
+    @AfterViews
+    void initDrawer()
+    {
+        String[] names = getResources().getStringArray(R.array.drawer);
+        NavigationDrawerAdapter adapter = new NavigationDrawerAdapter(this,names);
+        leftDrawer.setAdapter(adapter);
+    }
+
+    @ItemClick(R.id.main_left_drawer)
+    public void drawerItemClick(int position) {
+        if(position==1)
+        {
+            StatisticsActivity_.intent(this).start();
+        }
+    }
+
 
 	@Background
 	void getApps()
@@ -58,4 +85,6 @@ public class MainActivity extends Activity implements BaseView
 	{
 		return this;
 	}
+
+
 }
---
+++ CrittercismApp/src/main/java/intexsoft/by/crittercismapi/ui/adapters/NavigationDrawerAdapter.java
@@ -0,0 +1,55 @@
+package intexsoft.by.crittercismapi.ui.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import intexsoft.by.crittercismapi.R;
+
+/**
+ * Created by vadim on 24.07.2014.
+ */
+
+
+public class NavigationDrawerAdapter extends ArrayAdapter<String>
+{
+    private final String[] mItems;
+    private LayoutInflater mInflater;
+
+    public NavigationDrawerAdapter(Context context, String[] items)
+    {
+        super(context, R.layout.drawer_item, items);
+        this.mItems = items;
+        mInflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
+
+        ViewHolder viewHolder;
+        if (convertView == null)
+        {
+            viewHolder = new ViewHolder();
+            convertView = mInflater.inflate(R.layout.drawer_item, parent, false);
+            viewHolder.mTextView=(TextView)convertView.findViewById(R.id.tvDrawer);
+            convertView.setTag(viewHolder);
+        }
+        else
+        {
+            viewHolder=(ViewHolder)convertView.getTag();
+        }
+        viewHolder.mTextView.setText(mItems[position]);
+
+
+        return convertView;
+    }
+
+    public static class ViewHolder
+    {
+       TextView mTextView;
+    }
+}
