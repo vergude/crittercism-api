05bb430e4f23fc415899c326c8cf5daa86c1c9d5
Tesla <nova888411@gmail.com>
2014-07-28 17:03:30 +0400

    Fixed post request

--- CrittercismApp/src/main/java/intexsoft/by/crittercismapi/data/facade/RemoteFacade.java
+++ CrittercismApp/src/main/java/intexsoft/by/crittercismapi/data/facade/RemoteFacade.java
@@ -3,33 +3,18 @@ package intexsoft.by.crittercismapi.data.facade;
 import android.content.Context;
 import android.util.Log;
 
-import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import org.androidannotations.annotations.EBean;
 import org.androidannotations.annotations.RootContext;
 import org.androidannotations.annotations.rest.RestService;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.web.client.RestTemplate;
 
-import java.io.IOException;
 import java.util.HashMap;
 
 import intexsoft.by.crittercismapi.Constants;
-import intexsoft.by.crittercismapi.CrittercismApplication;
 import intexsoft.by.crittercismapi.data.remote.request.GraphRequest;
 import intexsoft.by.crittercismapi.data.remote.request.GraphRequestInternal;
 import intexsoft.by.crittercismapi.data.remote.response.AppSummaryData;
 import intexsoft.by.crittercismapi.data.remote.response.GraphResponse;
 import intexsoft.by.crittercismapi.data.remote.service.CrittercismAPIService;
-import intexsoft.by.crittercismapi.settings.SettingsFacade;
 import intexsoft.by.crittercismapi.utils.ThreadUtils;
 
 
@@ -48,7 +33,8 @@ public class RemoteFacade
 		return RemoteFacade_.getInstance_(context);
 	}
 
-	public void getApps() {
+	public void getApps()
+    {
 		ThreadUtils.checkAndThrowIfUIThread();
 
         HashMap<String,AppSummaryData> response = remoteService.getApps();
@@ -56,7 +42,8 @@ public class RemoteFacade
         Log.d("**********", response.size() + "");
 	}
 
-    public void getErrorGraph() {
+    public void getErrorGraph()
+    {
         ThreadUtils.checkAndThrowIfUIThread();
 
         HashMap<String,AppSummaryData> responseApp = remoteService.getApps();
@@ -69,51 +56,6 @@ public class RemoteFacade
 
         graphRequest.setParams(graphRequestInternal);
 
-		ObjectMapper mapper = new ObjectMapper();
-		String jsonString = "";
-
-
-
-		try
-		{
-			jsonString = mapper.writeValueAsString(graphRequest);
-			Log.d("**********", jsonString);
-		}
-		catch (JsonProcessingException e)
-		{
-			Log.d("*****error*****","Errrrrorrrrrrr");
-			e.printStackTrace();
-		}
-//
-//		String response = remoteService.getErrorGraph(jsonString);
-
-		SettingsFacade settingsFacade = SettingsFacade.getInstance(CrittercismApplication.getApplication());
-
-		// Set the Content-Type header
-		HttpHeaders requestHeaders = new HttpHeaders();
-		requestHeaders.setContentType(new MediaType("application","json"));
-		requestHeaders.add("Authorization", "Bearer " + settingsFacade.getToken());
-		HttpEntity<String> requestEntity = new HttpEntity<String>(jsonString, requestHeaders);
-
-		// Create a new RestTemplate instance
-		RestTemplate restTemplate = new RestTemplate();
-
-		// Add the Jackson and String message converters
-		restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
-
-		// Make the HTTP POST request, marshaling the request to JSON, and the response to a String
-		ResponseEntity<String> responseEntity = restTemplate.exchange("https://developers.crittercism.com:443/v1.0/errorMonitoring/graph",
-				HttpMethod.POST, requestEntity, String.class);
-		String response = responseEntity.getBody();
-        Log.d("**********", response + "");
-
-        JsonFactory factory = new JsonFactory();
-        ObjectMapper mapper2 = new ObjectMapper(factory);
-        TypeReference<GraphResponse> typeRef = new TypeReference<GraphResponse>() {};
-        try {
-            GraphResponse graphResponse = mapper2.readValue(response, typeRef);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        GraphResponse graphResponse = remoteService.getErrorGraph(graphRequest);
     }
 }
--- CrittercismApp/src/main/java/intexsoft/by/crittercismapi/data/remote/request/GraphRequestInternal.java
+++ CrittercismApp/src/main/java/intexsoft/by/crittercismapi/data/remote/request/GraphRequestInternal.java
@@ -1,6 +1,10 @@
 package intexsoft.by.crittercismapi.data.remote.request;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.io.Serializable;
+
+import intexsoft.by.crittercismapi.data.remote.response.Filter;
 
 /**
  * Created by Евгений on 24.07.2014.
@@ -8,48 +12,60 @@ import java.io.Serializable;
 public class GraphRequestInternal implements Serializable
 {
     private String appId;
-    //private String [] applds;
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    private String [] applds;
     private String graph;
     private int duration;
-    //private Filter filters;
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    private Filter filters;
 
-//    public String[] getApplds() {
-//        return applds;
-//    }
-//
-//    public void setApplds(String[] applds) {
-//        this.applds = applds;
-//    }
+    public String[] getApplds()
+    {
+        return applds;
+    }
 
-//   public Filter getFilters() {
-//        return filters;
-//    }
+    public void setApplds(String[] applds)
+    {
+        this.applds = applds;
+    }
 
-//    public void setFilters(Filter filters) {
-//        this.filters = filters;
-//    }
+    public Filter getFilters()
+    {
+        return filters;
+    }
 
-    public String getAppId() {
+    public void setFilters(Filter filters)
+    {
+        this.filters = filters;
+    }
+
+    public String getAppId()
+    {
         return appId;
     }
 
-    public void setAppId(String appId) {
+    public void setAppId(String appId)
+    {
         this.appId = appId;
     }
 
-    public String getGraph() {
+    public String getGraph()
+    {
         return graph;
     }
 
-    public void setGraph(String graph) {
+    public void setGraph(String graph)
+    {
         this.graph = graph;
     }
 
-    public int getDuration() {
+    public int getDuration()
+    {
         return duration;
     }
 
-    public void setDuration(int duration) {
+    public void setDuration(int duration)
+    {
         this.duration = duration;
     }
 }
